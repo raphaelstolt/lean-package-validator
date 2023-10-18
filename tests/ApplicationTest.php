e521f83dd21bd2596cc0a89939343a9fc0cff272
Raphael Stolt <raphael.stolt@gmail.com>
2023-10-18 15:00:08 +0200

    Checks if expected commands exist

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -6,7 +6,7 @@ namespace Stolt\LeanPackage\Tests;
 
 use PHPUnit\Framework\TestCase as PHPUnit;
 
-class ApplicationTest extends PHPUnit
+final class ApplicationTest extends PHPUnit
 {
     /**
      * @test
@@ -22,6 +22,29 @@ class ApplicationTest extends PHPUnit
             'Lean package validator',
             $output[1],
             'Expected application name not present.'
+        );
+        $this->assertEquals(0, $returnValue);
+    }
+
+    /**
+     * @test
+     * @group integration
+     */
+    public function expectedCommandsAreListed(): void
+    {
+        $binaryCommand = 'php bin/lean-package-validator list';
+
+        \exec($binaryCommand, $output, $returnValue);
+
+        $this->assertStringContainsString(
+            'init',
+            $output[17],
+            'Expected init command not listed.'
+        );
+        $this->assertStringContainsString(
+            'validate',
+            $output[19],
+            'Expected validate command not listed.'
         );
         $this->assertEquals(0, $returnValue);
     }
