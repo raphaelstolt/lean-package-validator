a3cda50c7e79f33fe0be8eff780e91dc2f6fd335
Raphael Stolt <raphael.stolt@gmail.com>
2024-05-16 08:16:18 +0200

    Utilises parallel runner

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,6 +2,7 @@
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $finder = Finder::create()
     ->in([__DIR__, __DIR__ . DIRECTORY_SEPARATOR . 'tests']);
@@ -22,5 +23,6 @@ $cacheDir = \getenv('HOME') ? \getenv('HOME') : __DIR__;
 $config = new Config();
 
 return $config->setRules($rules)
+    ->setParallelConfig(ParallelConfigFactory::detect())
     ->setFinder($finder)
     ->setCacheFile($cacheDir . '/.php-cs-fixer.cache');
