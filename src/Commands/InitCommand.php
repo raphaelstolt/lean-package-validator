70c329b30c489a4ae09ca3f5f993ab24f5abca0b
Raphael Stolt <raphael.stolt@gmail.com>
2025-02-18 08:36:34 +0100

    Marks commands as final

--- src/Commands/InitCommand.php
+++ src/Commands/InitCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class InitCommand extends Command
+final class InitCommand extends Command
 {
     private const DEFAULT_PRESET = 'PHP';
 
--- src/Commands/ValidateCommand.php
+++ src/Commands/ValidateCommand.php
@@ -23,7 +23,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ValidateCommand extends Command
+final class ValidateCommand extends Command
 {
     /**
      * Default glob pattern file.
